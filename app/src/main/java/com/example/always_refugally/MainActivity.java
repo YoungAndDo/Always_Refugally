8838293975f9ebaa72842f95e8fe953e2d57068e
YoungAndDo <yd199323@gmail.com>
2016-11-17 17:43:48 +0900

    Preparing for using GPS info

--- app/src/main/java/com/example/always_refugally/MainActivity.java
+++ app/src/main/java/com/example/always_refugally/MainActivity.java
@@ -1,20 +1,52 @@
 package com.example.always_refugally;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String urlPath ="http://http://yd1993.dothome.co.kr/";
+
+    @Bind(R.id.search_tap)
+    AutoCompleteTextView textView;
+
+    @Bind(R.id.button)
+    Button btn_search;
+
+    @Bind(R.id.setLocation)
+    Button btn_location;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
         String[] product_array = getResources().getStringArray(R.array.product_name);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, product_array);
-        AutoCompleteTextView textView = (AutoCompleteTextView) findViewById(R.id.search_tap);
         textView.setAdapter(adapter);
+
+        btn_search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, ListActivity.class);
+                startActivity(i);
+            }
+        });
+
+        btn_location.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
