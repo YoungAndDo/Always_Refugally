4c27c319b5bd55ae18358bf7f80dfc8f06cec422
YoungAndDo <yd199323@gmail.com>
2016-11-17 16:12:24 +0900

    Add ListActivity

--- app/src/main/java/com/example/always_refugally/MainActivity.java
+++ app/src/main/java/com/example/always_refugally/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.always_refugally;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] product_array = getResources().getStringArray(R.array.product_name);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, product_array);
+        AutoCompleteTextView textView = (AutoCompleteTextView) findViewById(R.id.search_tap);
+        textView.setAdapter(adapter);
     }
 }
