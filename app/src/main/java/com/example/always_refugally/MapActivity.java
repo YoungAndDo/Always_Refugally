309835c3344abdbbec6c0220860fa6aaa1a756de
noriadne <robert_8@naver.com>
2016-12-06 03:46:26 +0900

    add express location to address

--- app/src/main/java/com/example/always_refugally/MapActivity.java
+++ app/src/main/java/com/example/always_refugally/MapActivity.java
@@ -5,16 +5,21 @@ import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import net.daum.mf.map.api.MapPoint;
 import net.daum.mf.map.api.MapReverseGeoCoder;
 import net.daum.mf.map.api.MapView;
 
 import static com.example.always_refugally.R.id.button2;
+import static com.example.always_refugally.R.id.button3;
+import static com.example.always_refugally.R.id.texttext;
 
 
 public class MapActivity extends FragmentActivity
         implements MapView.OpenAPIKeyAuthenticationResultListener, MapView.MapViewEventListener, MapView.CurrentLocationEventListener, MapReverseGeoCoder.ReverseGeoCodingResultListener {
+    private MapReverseGeoCoder mReverseGeoCoder = null;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -25,15 +30,24 @@ public class MapActivity extends FragmentActivity
 //다음이 제공하는 MapView객체 생성 및 API Key 설정
         final MapView mapView = new MapView(this);
         mapView.setDaumMapApiKey("1581af30c6260a7eba804e18e5319ddb");
-
         ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
         mapViewContainer.addView(mapView);
         mapView.setCurrentLocationEventListener(this);
         Button btn = (Button) findViewById(button2);
+        Button btn2 = (Button) findViewById(button3);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 mapView.setCurrentLocationTrackingMode(MapView.CurrentLocationTrackingMode.TrackingModeOnWithoutHeading);
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mReverseGeoCoder = new MapReverseGeoCoder("1581af30c6260a7eba804e18e5319ddb", mapView.getMapCenterPoint(), MapActivity.this, MapActivity.this);
+                mReverseGeoCoder.startFindingAddress();
+                mapView.setCurrentLocationTrackingMode(MapView.CurrentLocationTrackingMode.TrackingModeOff);
+                mapView.setShowCurrentLocationMarker(false);
             }
         });
     }
@@ -90,12 +104,13 @@ public class MapActivity extends FragmentActivity
 
     @Override
     public void onReverseGeoCoderFoundAddress(MapReverseGeoCoder mapReverseGeoCoder, String s) {
+        onFinishReverseGeoCoding(s);
 
     }
 
     @Override
     public void onReverseGeoCoderFailedToFindAddress(MapReverseGeoCoder mapReverseGeoCoder) {
-
+        onFinishReverseGeoCoding("Fail");
     }
 
     @Override
@@ -116,5 +131,11 @@ public class MapActivity extends FragmentActivity
     @Override
     public void onCurrentLocationUpdateCancelled(MapView mapView) {
 
+    }
+
+    private void onFinishReverseGeoCoding(String result) {
+        TextView localtext = (TextView) findViewById(texttext);
+        Toast.makeText(MapActivity.this, "현재 위치 : " + result, Toast.LENGTH_SHORT).show();
+        localtext.setText(result);
     }
 }
