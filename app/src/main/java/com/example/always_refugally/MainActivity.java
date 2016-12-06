be27cc8a62003b28b22701c5dd79905ed2082250
YoungAndDo <yd199323@gmail.com>
2016-12-06 23:36:52 +0900

    Edit MainActivity, Add ResultActivity

--- app/src/main/java/com/example/always_refugally/MainActivity.java
+++ app/src/main/java/com/example/always_refugally/MainActivity.java
@@ -9,12 +9,17 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.google.zxing.client.android.integration.IntentIntegrator;
 import com.google.zxing.client.android.integration.IntentResult;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -45,15 +50,81 @@ public class MainActivity extends AppCompatActivity {
     @Bind(R.id.button_barcode)
     Button barcode;
 
+    @Bind(R.id.List1_layout)
+    RelativeLayout list1;
+
+    @Bind(R.id.List2_layout)
+    RelativeLayout list2;
+
+    @Bind(R.id.List3_layout)
+    RelativeLayout list3;
+
+    @Bind(R.id.list1_plus)
+    ImageButton list1_plus;
+
+    @Bind(R.id.list1_minus)
+    ImageButton list1_minus;
+
+    @Bind(R.id.list1_name)
+    TextView list1_name;
+
+    @Bind(R.id.list1_count)
+    TextView list1_count;
+
+    @Bind(R.id.list1_pic)
+    ImageView list1_pic;
+
+    @Bind(R.id.list2_plus)
+    ImageButton list2_plus;
+
+    @Bind(R.id.list2_minus)
+    ImageButton list2_minus;
+
+    @Bind(R.id.list2_name)
+    TextView list2_name;
+
+    @Bind(R.id.list2_count)
+    TextView list2_count;
+
+    @Bind(R.id.list2_pic)
+    ImageView list2_pic;
+
+    @Bind(R.id.list3_plus)
+    ImageButton list3_plus;
+
+    @Bind(R.id.list3_minus)
+    ImageButton list3_minus;
+
+    @Bind(R.id.list3_name)
+    TextView list3_name;
+
+    @Bind(R.id.list3_count)
+    TextView list3_count;
+
+    @Bind(R.id.list3_pic)
+    ImageView list3_pic;
+
+
+
     Intent i;
     SpeechRecognizer mRecognizer;
     ArrayList<String> mResult;
+    Map<String,Integer> item;
+    int count;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        list1.setVisibility(View.GONE);
+        list2.setVisibility(View.GONE);
+        list3.setVisibility(View.GONE);
+        item = new HashMap<String,Integer>();
+        count = 0;
+
+
 
         setSupportActionBar(myToolbar);
 
@@ -70,10 +141,10 @@ public class MainActivity extends AppCompatActivity {
         btn_search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                i = new Intent(MainActivity.this, ListActivity.class);
-
-                i.putExtra("name", editText.getText().toString());
-                startActivity(i);
+//                i = new Intent(MainActivity.this, ListActivity.class);
+//
+//                i.putExtra("name", editText.getText().toString());
+//                startActivity(i);
             }
         });
 
@@ -104,7 +175,159 @@ public class MainActivity extends AppCompatActivity {
                 IntentIntegrator.initiateScan(MainActivity.this);
             }
         });
+
+        list1_plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String tmp;
+                int count;
+                tmp = list1_count.getText().toString();
+                count = Integer.parseInt(tmp);
+                count++;
+                item.put(list1_name.getText().toString(),count);
+                tmp = String.valueOf(count);
+                list1_count.setText(tmp);
+            }
+        });
+        list1_minus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String tmp;
+                int c;
+                tmp = list1_count.getText().toString();
+                c = Integer.parseInt(tmp);
+                c--;
+                item.put(list1_name.getText().toString(),c);
+                tmp = String.valueOf(c);
+                list1_count.setText(tmp);
+                if(c == 0)
+                {
+                    item.remove(list1_name.getText().toString());
+                    if(count == 3)
+                    {
+                        list3.setVisibility(View.GONE);
+                        list1_name.setText(list2_name.getText().toString());
+                        list1_count.setText(list2_count.getText().toString());
+                        list2_name.setText(list3_name.getText().toString());
+                        list2_count.setText(list3_count.getText().toString());
+                    }
+                    else if(count == 2)
+                    {
+                        list2.setVisibility(View.GONE);
+                        list1_name.setText(list2_name.getText().toString());
+                        list1_count.setText(list2_count.getText().toString());
+                    }
+                    else
+                    {
+                        list1.setVisibility(View.GONE);
+                    }
+                    count--;
+                }
+            }
+        });
+        list2_plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String tmp;
+                int count;
+                tmp = list2_count.getText().toString();
+                count = Integer.parseInt(tmp);
+                count++;
+                item.put(list2_name.getText().toString(),count);
+                tmp = String.valueOf(count);
+                list2_count.setText(tmp);
+            }
+        });
+        list2_minus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String tmp;
+                int c;
+                tmp = list2_count.getText().toString();
+                c = Integer.parseInt(tmp);
+                c--;
+                item.put(list2_name.getText().toString(),c);
+                tmp = String.valueOf(c);
+                list2_count.setText(tmp);
+                if(c == 0)
+                {
+                    item.remove(list2_name.getText().toString());
+                    if(count == 3)
+                    {
+                        list3.setVisibility(View.GONE);
+                        list2_name.setText(list3_name.getText().toString());
+                        list2_count.setText(list3_count.getText().toString());
+                    }
+                    else
+                    {
+                        list2.setVisibility(View.GONE);
+                    }
+                    count--;
+                }
+            }
+        });
+        list3_plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String tmp;
+                int count;
+                tmp = list3_count.getText().toString();
+                count = Integer.parseInt(tmp);
+                count++;
+                item.put(list3_name.getText().toString(),count);
+                tmp = String.valueOf(count);
+                list3_count.setText(tmp);
+            }
+        });
+        list3_minus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String tmp;
+                int c;
+                tmp = list3_count.getText().toString();
+                c = Integer.parseInt(tmp);
+                c--;
+                item.put(list3_name.getText().toString(),c);
+                tmp = String.valueOf(c);
+                list3_count.setText(tmp);
+                if(c == 0)
+                {
+                    item.remove(list3_name.getText().toString());
+                    list3.setVisibility(View.GONE);
+                    count--;
+                }
+            }
+        });
+
     }
+
+    void putitem()
+    {
+        String name = editText.getText().toString();
+        item.put(name, 1);
+        if(count == 0)
+        {
+            list1.setVisibility(View.VISIBLE);
+            list1_name.setText(name);
+            list1_count.setText("1");
+            count++;
+        }
+        else if(count == 1)
+        {
+            list2.setVisibility(View.VISIBLE);
+            list2_name.setText(name);
+            list2_count.setText("1");
+            count++;
+        }
+        else if(count == 2)
+        {
+            list3.setVisibility(View.VISIBLE);
+            list3_name.setText(name);
+            list3_count.setText("1");
+            count++;
+        }
+    }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
